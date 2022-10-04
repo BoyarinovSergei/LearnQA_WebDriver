c1a34aeb8986264a93fb78a2aaeb87b01eaa460a
Sergei Boyarinov <s.boyarinov@telecontact.ru>
2022-10-04 14:53:46 +0300

    [x] Задание 3. Сделайте сценарий логина

--- src/test/java/TestFirstStart.java
+++ src/test/java/TestFirstStart.java
@@ -1,6 +1,8 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,12 +16,21 @@ public class TestFirstStart {
     @Before
     public void setUp(){
         driver = new ChromeDriver();
-        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
     @Test
     public void openBrowser(){
         driver.get("https://github.com/");
+    }
+
+    @Test
+    public void logIn(){
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        Assert.assertTrue(driver.findElement(By.id("sidebar")).isDisplayed());
     }
 
     @After
